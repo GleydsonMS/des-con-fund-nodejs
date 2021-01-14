6caedf4c16e58e21476a24271d95e6f2e07d9ec6
Gleyson Miranda Souza <gleydson.m.s.1993@gmail.com>
2021-01-14 19:22:50 -0300

    finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,10 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +19,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeFilter = this.transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+
+    const income = incomeFilter.reduce((acc, cv) => acc + cv.value, 0);
+
+    const outcomeFilter = this.transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+
+    const outcome = outcomeFilter.reduce((acc, cv) => acc + cv.value, 0);
+
+    const total = income - outcome;
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
